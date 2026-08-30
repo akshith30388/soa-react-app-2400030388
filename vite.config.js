4b9323e993167d69714eadfc22f41e7d12f3fbe9
akshith30388 <akshithpola36@gmail.com>
2026-08-30 22:05:36 +0530

    Configure GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/soa-react-app-2400030388/',
 })
